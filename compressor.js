e7312d55a7613f0d0a1c9bd218e3cefb4a91a434
mdatsev <martin.datsev.p@gmail.com>
2018-01-11 21:32:57 +0200

    added yaml loading

--- compressor.js
+++ compressor.js
@@ -1,6 +1,16 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+try {
+    var doc = yaml.safeLoad(fs.readFileSync('./tokens.yaml', 'utf8'));
+    console.log(doc);
+} catch (e) {
+    console.log(e);
+}
+
 let source = '';
 process.stdin.resume();
-process.stdin.on('data', function(buf) { source += buf.toString(); });
-process.stdin.on('end', function() {
+process.stdin.on('data', function (buf) { source += buf.toString(); });
+process.stdin.on('end', function () {
     console.log(source);
 });
