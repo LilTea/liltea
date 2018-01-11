ec0c8369974e4be9eb6da3becae79dcaa0e4c176
mdatsev <martin.datsev.p@gmail.com>
2018-01-11 23:51:25 +0200

    added parsing

--- engine.js
+++ engine.js
@@ -110,5 +110,7 @@ function increment(){
 function decrement(){
     stack[stack.length - 1]--;;
 }
-decrement();
-console.log(stack);
+
+exports.execute = function(command) {
+    eval(command+'()');
+}
