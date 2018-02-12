442a1b2a6c7252c325fef32bbfab906eb108237b
mdatsev <martin.datsev.p@gmail.com>
2018-02-13 01:14:50 +0200

    added runner tests

---
+++ lilTeaRunner.test.js
@@ -0,0 +1,10 @@
+const runLilTea = require('./lilTeaRunner').runLilTea;
+
+let engine = {deleteModificators: jest.fn()}
+
+test('calling the right engine function', () => {
+    let source = "+";
+    engine.test = jest.fn();
+    runLilTea(source, engine, () => "test");
+    expect(engine.test).toHaveBeenCalled();
+});
