0811029fb809222e024b5e10c9924be113225209
mdatsev <martin.datsev.p@gmail.com>
2018-02-13 17:51:42 +0200

    made use of the new class in liltea.js

--- liltea.js
+++ liltea.js
@@ -1,5 +1,6 @@
 require("./helpers.js").getInput(input => {
-    const engine = require("./engine");
+    const Engine = require("./engine").engine;
+    const engine = new Engine(console.log);
     const source = require("fs").readFileSync(process.argv[2]).toString();
     let stack = require("./inputParser").parseInput(input);
     engine.setStack(stack);
