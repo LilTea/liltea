e2091b7dae6321a233c12d804a5f6c9d162934cc
mdatsev <martin.datsev.p@gmail.com>
2018-02-23 19:35:43 +0200

    added more I/O tests

--- test/inputParser.test.js
+++ test/inputParser.test.js
@@ -10,3 +10,40 @@ test('multiline just numbers happy test', () => {
         [4, 5, 6]
     ]);
 });
+
+test('one number per line', () => {
+    let input = 
+`1
+2
+3`;
+    let stack = parseInput(input);
+    expect(stack).toEqual([
+        1,
+        2,
+        3
+    ]);
+});
+
+test('one number per line', () => {
+    let input = 
+`1
+2
+3`;
+    let stack = parseInput(input);
+    expect(stack).toEqual([
+        1,
+        2,
+        3
+    ]);
+});
+
+test('just strings', () => {
+    let input = 
+`a
+a b cde`;
+    let stack = parseInput(input);
+    expect(stack).toEqual([
+        'a',
+        ['a', 'b', 'cde']
+    ]);
+});
