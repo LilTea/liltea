f071ce97919c15d0e9ca6015b48c2f53e20da7db
mdatsev <martin.datsev.p@gmail.com>
2018-02-25 23:26:15 +0200

    getInput tests

---
+++ test/helpers.test.js
@@ -0,0 +1,15 @@
+const helpers = require("../lib/helpers")
+
+test('just strings', () => {
+    const stdin = require('mock-stdin').stdin();
+    
+    const callback = jest.fn();
+
+    helpers.getInput(callback);
+    
+    stdin.send("a b c\n1 2 3");
+    stdin.end();
+
+    expect(callback).toHaveBeenCalledWith("a b c\n1 2 3")
+});
+
