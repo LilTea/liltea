d9fe9e5ca8641134d54f4b633f0fc9939d6db380
Vladislav <vladdislav.georgiev@gmail.com>
2018-04-02 00:00:31 +0300

    test refactoring

--- test/engine.test.js
+++ test/engine.test.js
@@ -1,190 +1,189 @@
 const Engine = require('../lib/engine').engine;
 eng = new Engine();
-test('add', () => {
-    eng.setStack([1, 2]);
-    eng.do('add');
-    expect(eng.stack).toEqual([3]);
-});
-test('subtraction', ()=> {
-    eng.setStack([3,4]);
-    eng.do('subtraction');
-    expect(eng.stack).toEqual([-1]);
+
+describe('basic number operation',()=>{
+    beforeEach(()=>{
+        eng.setStack([3,4,8])
+    })
+    test('add', () => {
+        eng.do('add');
+        expect(eng.stack).toEqual([3,12]);
+    });
+    test('subtraction', ()=> {
+        eng.do('subtraction');
+        expect(eng.stack).toEqual([3,-4]);
+    })
+    test('mult', ()=> {
+        eng.do('mult');
+        expect(eng.stack).toEqual([3,32]);
+    })
+    test('divide', ()=>{
+        eng.do('divide');
+        expect(eng.stack).toEqual([3,0.5]);
+    })
+    test('square', ()=>{
+        eng.do('square');
+        expect(eng.stack).toEqual([3,4,64])
+    })
+    test('dup', ()=>{
+        eng.do('dup');
+        expect(eng.stack).toEqual([3,4,8,8])
+    })
+    test('decrement', ()=>{
+        eng.do('decrement') 
+        expect(eng.stack).toEqual([3,4,7])
+    })
+    test('increment', ()=>{
+        eng.do('increment') 
+        expect(eng.stack).toEqual([3,4,9])
+    })
+    test('fact', ()=>{
+        eng.do('fact') 
+        expect(eng.stack).toEqual([3,4,40320])
+    })
 })
-test('mult', ()=> {
-    eng.setStack([3,4,6]);
-    eng.do('mult');
-    expect(eng.stack).toEqual([3,24]);
+describe('stack sorts with numbers', ()=>{
+    beforeEach(()=>{
+        eng.setStack([[20,-1,5,7,12,6]])
+    })
+    test('ascendingSort with numbers', () => {
+        eng.do('ascendingSort')
+        expect(eng.stack).toEqual([[-1,5,6,7,12,20]])
+    })
+    test('descendingSort with numbers', () => {
+        eng.do('descendingSort')
+        expect(eng.stack).toEqual([[20,12,7,6,5,-1]])
+    })
 })
-test('divide', ()=>{
-    eng.setStack([3,4,20]);
-    eng.do('divide');
-    expect(eng.stack).toEqual([3,0.2]);
+describe('sorts with strings', ()=>{
+    beforeEach(()=>{
+        eng.setStack([['c','d','a','w']])
+    })
+    test('ascendingSort with string', () => {
+        eng.do('ascendingSort')
+        expect(eng.stack).toEqual([['a','c','d','w']])
+    })
+    
+    test('descendingSort with string', () => {
+        eng.do('descendingSort')
+        expect(eng.stack).toEqual([['w','d','c','a']])
+    })
 })
-test('ascendingSort with numbers', () => {
-    eng.setStack([[20,-1,5,7,12,6]])
-    eng.do('ascendingSort')
-    expect(eng.stack).toEqual([[-1,5,6,7,12,20]])
+describe('comparisments', ()=>{
+    beforeEach(()=>{
+        eng.setStack(['asd', 3 ,4 ,5])
+    })
+    test('compare', ()=>{
+        eng.do('compare') 
+        expect(eng.stack).toEqual(['asd',3,0])
+    })
+    test('bigger', ()=>{
+        eng.do('bigger') 
+        expect(eng.stack).toEqual(['asd',3,true])
+    })
+    test('less', ()=>{
+        eng.do('less') 
+        expect(eng.stack).toEqual(['asd',3,false])
+    })
 })
-test('ascendingSort with string', () => {
-    eng.setStack([['c','d','a','w']])
-    eng.do('ascendingSort')
-    expect(eng.stack).toEqual([['a','c','d','w']])
+describe('bitwise operations', ()=>{
+    beforeEach(()=>{
+        eng.setStack(['asd', 3 ,5,6])
+    })
+    test('bitswiseAnd', ()=>{
+        eng.do('bitwiseAnd') 
+        expect(eng.stack).toEqual(['asd',3, 4])
+    })
+    test('bitswiseAnd', ()=>{
+        eng.do('bitwiseOr') 
+        expect(eng.stack).toEqual(['asd',3, 7])
+    })
+    test('bitswiseXor', ()=>{
+        eng.do('bitwiseXor') 
+        expect(eng.stack).toEqual(['asd',3, 3])
+    })
 })
-test('descendingSort with numbers', () => {
-    eng.setStack([[20,-1,5,7,12,6]])
-    eng.do('descendingSort')
-    expect(eng.stack).toEqual([[20,12,7,6,5,-1]])
+describe('casts', ()=>{
+    beforeEach(()=>{
+        eng.setStack(['asd', 3 ,[4 ,5],'56'])
+    })
+    test('toInteger', ()=>{
+        eng.do('toInteger') 
+        expect(eng.stack).toEqual(['asd',3,[4,5],56])
+    })
+    test('toArray', ()=>{
+        eng.do('toArray') 
+        expect(eng.stack).toEqual(['asd',3,[4,5],['56']])
+    })
+    test('toArray', ()=>{
+        eng.do('toArray') 
+        expect(eng.stack).toEqual(['asd',3,[4,5],['56']])
+    })
 })
-test('descendingSort with string', () => {
-    eng.setStack([['c','d','a','w']])
-    eng.do('descendingSort')
-    expect(eng.stack).toEqual([['w','d','c','a']])
+describe('trigonometric functions', ()=>{
+    beforeEach(()=>{
+        eng.setStack([3,4,90]); 
+    })
+    test('sin', ()=>{
+        eng.do('sin');
+        expect(eng.stack).toEqual([3,4,1])
+    })
+    test('cos', ()=>{
+        eng.do('cos');
+        expect(eng.stack).toEqual([3,4,0])
+    })
 })
-test('square', ()=>{
-    eng.setStack([3,4,8]);
-    eng.do('square');
-    expect(eng.stack).toEqual([3,4,64])
+describe('variables functionality', ()=>{
+    beforeEach(()=>{
+        eng.setStack([10, 0, 1])
+        eng.setVar(2);
+    })
+    test('var set and get', ()=>{
+        eng.getVar(2);
+        expect(eng.stack).toEqual([10,0,1]);
+    })
+    test('checking the stack after set', ()=>{
+        expect(eng.stack).toEqual([10,0]);
+    })
+    test('checking the def values after get', ()=>{
+        eng.getVar(2);
+        eng.getVar(2);
+        expect(eng.stack).toEqual([10,0,1,Math.PI]);
+    });
 })
-test('dup', ()=>{
-    eng.setStack([3,4,8]);
-    eng.do('dup');
-    expect(eng.stack).toEqual([3,4,8,8])
+describe('rounding and aproximations', ()=>{
+    beforeEach(()=>{
+        eng.setStack(['asd', 3 ,[4 ,5],5.2])
+    })
+    test('ceil', ()=>{
+        eng.do('ceil') 
+        expect(eng.stack).toEqual(['asd',3,[4,5],6])
+    })
+    test('floor', ()=>{
+        eng.do('floor') 
+        expect(eng.stack).toEqual(['asd',3,[4,5],5])
+    })
 })
-test('sin', ()=>{
-    eng.setStack([3,4,Math.PI/2]);
-    eng.do('sin');
-    expect(eng.stack).toEqual([3,4,1])
-})
-test('cos', ()=>{
-    eng.setStack([3,4,Math.PI]);
-    eng.do('cos');
-    expect(eng.stack).toEqual([3,4,-1])
-})
-test('empty', ()=>{
-    eng.setStack([3,4,'asd']);
-    eng.do('empty')
-    expect(eng.stack).toEqual([])
-})
-test('stackLenght', ()=>{
-    eng.setStack([3,4,'asd']);
-    eng.do('stackLength')
-    expect(eng.stack).toEqual([3,4,'asd',3])
-})
-test('compare', ()=>{
-    eng.setStack(['asd', 3 ,4 ,5])
-    eng.do('compare') 
-    expect(eng.stack).toEqual(['asd',3,0])
+describe('stack status', ()=>{
+    beforeEach(()=>{
+        eng.setStack([10,"asd",false])
+    })
+    test('empty', ()=>{
+        eng.do('empty')
+        expect(eng.stack).toEqual([])
+    })
+    test('stackLenght', ()=>{
+        eng.do('stackLength')
+        expect(eng.stack).toEqual([10,"asd",false,3])
+    })
+    test('popCondition', ()=>{
+        eng.setStack([10, false])
+        expect(eng.popCondition()).toEqual(false)
+        expect(eng.stack).toEqual([10])
+    })
 })
 test('element', ()=>{
     eng.setStack(['asd', 3 ,[4 ,5],1])
     eng.do('element') 
     expect(eng.stack).toEqual(['asd',3,[4,5],1,5])
-})
-test('toString', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],1])
-    eng.do('toString') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],'1'])
-})
-test('toInteger', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],'56'])
-    eng.do('toInteger') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],56])
-})
-test('toArray', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],'56'])
-    eng.do('toArray') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],['56']])
-})
-test('toArray', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],'56'])
-    eng.do('toArray') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],['56']])
-})
-test('fact', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],5])
-    eng.do('fact') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],120])
-})
-test('ceil', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],5.2])
-    eng.do('ceil') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],6])
-})
-test('floor', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],5.8])
-    eng.do('floor') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],5])
-})
-test('decrement', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],6])
-    eng.do('decrement') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],5])
-})
-test('increment', ()=>{
-    eng.setStack(['asd', 3 ,[4 ,5],6])
-    eng.do('increment') 
-    expect(eng.stack).toEqual(['asd',3,[4,5],7])
-})
-
-test('popCondition', ()=>{
-    eng.setStack([10, false])
-    expect(eng.popCondition()).toEqual(false)
-    expect(eng.stack).toEqual([10])
-})
-
-test('var set and get', ()=>{
-    eng.setStack([10, 0, 1])
-    eng.setVar(0);
-    eng.getVar(0);
-    expect(eng.stack).toEqual([10,0,1]);
-})
-test('checking the stack after set', ()=>{
-    eng.setStack([10, 0, 1])
-    eng.setVar(0);
-    expect(eng.stack).toEqual([10,0]);
-})
-test('checking the stack after set and get', ()=>{
-    eng.setStack([10,0,1])
-    eng.setVar(2);
-    eng.getVar(2);
-    expect(eng.stack).toEqual([10,0,1]);
-});
-test('checking the def values after get', ()=>{
-    eng.setStack([10,0,1])
-    eng.setVar(2);
-    eng.getVar(2);
-    eng.getVar(2);
-    expect(eng.stack).toEqual([10,0,1,Math.PI]);
-});
-test('checking the def values after get', ()=>{
-    eng.setStack([10,0,1])
-    eng.setVar(2);
-    eng.getVar(2);
-    eng.getVar(2);
-    expect(eng.stack).toEqual([10,0,1,Math.PI]);
-});
-test('bigger', ()=>{
-    eng.setStack(['asd', 3 ,5 , 6])
-    eng.do('bigger') 
-    expect(eng.stack).toEqual(['asd',3,true])
-})
-test('less', ()=>{
-    eng.setStack(['asd', 3 ,5,6])
-    eng.do('less') 
-    expect(eng.stack).toEqual(['asd',3,false])
-})
-test('bitswiseAnd', ()=>{
-    eng.setStack(['asd', 3 ,5,6])
-    eng.do('bitwiseAnd') 
-    expect(eng.stack).toEqual(['asd',3, 4])
-})
-test('bitswiseAnd', ()=>{
-    eng.setStack(['asd', 3 ,5 ,6])
-    eng.do('bitwiseOr') 
-    expect(eng.stack).toEqual(['asd',3, 7])
-})
-test('bitswiseAnd', ()=>{
-    eng.setStack(['asd', 3 ,5 ,6])
-    eng.do('bitwiseXor') 
-    expect(eng.stack).toEqual(['asd',3, 3])
 })
