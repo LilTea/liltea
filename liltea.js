c0315d5a6ad41526581a10bd70ed804e362ea610
mdatsev <martin.datsev.p@gmail.com>
2018-04-09 13:23:12 +0300

    hooked the literal completer into the pipeline

--- liltea.js
+++ liltea.js
@@ -1,16 +1,18 @@
 let helpers = require("./lib/helpers");
-function main(input)
-{
+
+function main(input) {
+    const config = require("./lib/config").loadConfigSync();
     const Engine = require("./lib/engine").engine;
     const engine = new Engine(s => process.stdout.write(s));
     const source = require("fs").readFileSync(process.argv[2]).toString();
     let stack = require("./lib/inputParser").parseInput(input);
     engine.setStack(stack);
-    require("./lib/lilTeaRunner").runLilTea(source, engine,
+    require("./lib/lilTeaRunner").runLilTea(
+        require("./lib/preLexer").completeLiterals(source, config.getSpecialTokens()), engine,
         require("./lib/converters").lil2jsToken,
-        require("./lib/config").loadConfigSync().getSpecialTokens());
+        config.getSpecialTokens());
 }
-if(process.argv[3])
+if (process.argv[3])
     main(helpers.getInputFromFile(process.argv[3]));
 else
     helpers.getInput(main);
